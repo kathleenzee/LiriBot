03ff63fe2edea089ec432df1f8039acc1bb67ffd
kathleenzee <kathleenzelony@gmail.com>
2017-04-06 15:26:01 -0400

    all the stuffs

---
+++ keys.js
@@ -0,0 +1,8 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: 'WZKo22bK8kkhuyMaswIBA3yGG',
+  consumer_secret: '1w9Fr01QYUk1o1P6Q6v8r9lNf9V47ZQ2mTFG9Ak22CmmSf9ePl',
+  access_token_key: '32443382-GiRzV0nGiRMOmbAQgR2EYWesdH5P3Xw1GHhZifPgt',
+  access_token_secret: 'GKBpoHjw70SpefD334BYdbd6KmTY91PqRPz2knUIbTq0c',
+}
